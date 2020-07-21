944b263e5538c94f02638e9fad7640e335ee0392
arovinsky <cshelp@WWLL0077.wpspublish.com>
2020-07-20 18:31:52 -0700

    completed hamburger menu show hide

--- Web1-Workbook/single-page-app/SPA-In-Class/src/App/Nav.jsx
+++ Web1-Workbook/single-page-app/SPA-In-Class/src/App/Nav.jsx
@@ -1,16 +1,54 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components';
 import {NavLink } from 'react-router-dom'
 import { mq } from '../common/mediaQueries.js'
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Nav = () => {
+
+    const [showMenu, showMenuSet]= useState(false);
+
+    const toggleMenu = () => {
+        console.log ('you clicked a hamburger')
+        showMenuSet(!showMenu)
+    }
+
+    // use effect is for lifecycle events
+    // empty bracket triggers component mounted
+
+    useEffect (()=>{
+        console.log("I have mounted")
+        const handleWindowSizeChange = () => {
+            const isItMobile = window.matchMedia(`(max-width:499px)`)
+            console.log (isItMobile)
+            showMenuSet(!isItMobile.matches)
+        }
+        handleWindowSizeChange();
+        window.addEventListener('resize',handleWindowSizeChange)
+
+        // Clean up listener on dismount
+        return () => {
+            window.removeEventListener('resize',handleWindowSizeChange)
+        }
+    }, []); // straight braces means component mounted
+
     return (
         <NavStyled>
         <div className="nested-wrapper">
-            <NavLink  to="/" exact>Welcome</NavLink>
-            <NavLink  to="/services">Services</NavLink>
-            <NavLink  to="/contacts">Contacts</NavLink>
-            <NavLink  to="/login">Login</NavLink>
+            <div className="hamburger" onClick={ toggleMenu }>
+                <FontAwesomeIcon icon={faBars} ></FontAwesomeIcon>                
+            </div>
+                {
+                showMenu && 
+                <div class="links">
+                    <NavLink  to="/" exact>Welcome</NavLink>
+                    <NavLink  to="/services">Services</NavLink>
+                    <NavLink  to="/contacts">Contacts</NavLink>
+                    <NavLink  to="/login">Login</NavLink>
+                </div>
+                }
         </div>
         </NavStyled>
     )
@@ -21,6 +59,27 @@ export default Nav;
 const NavStyled = styled.nav`
     background-color: teal;
     color: white;
+
+    .hamburger {
+        position: absolute;
+        right: 20px;
+        top: 20px;
+        cursor: pointer;
+
+        svg {
+            color: red;
+            width: 40px;
+            height: auto;
+        }
+
+        display: block;
+
+        @media ${mq.tablet} {
+            display: none;
+        }
+    }
+
+
     text-align: center;
     a {
         display: inline-block;
